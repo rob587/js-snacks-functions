ec7a5489981bd4935d26ac549d2377aef711db5a
rob587 <robertocamm02@yahoo.com>
2025-07-08 15:24:00 +0200

    completato secondo compito

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstLetter(){
+    return names.map(name => {
+        //diviso il nome in parole//
+        const words = name.split(' ');
+        //estraggo la prima lettera di ogni parola e la unisco
+        const initials = words.map(word => word.charAt(0)).join(' ')
+        return initials;
+    })
+}
+
+  
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetter())
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
