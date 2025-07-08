39395462ccecffbcdfcb82009f3a5a9c488e230c
rob587 <robertocamm02@yahoo.com>
2025-07-08 14:46:33 +0200

    completato

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,9 +5,14 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function salute(name){
+    return 'ciao'+ ' '+userName
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+salute();
+console.log(salute(name))
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
