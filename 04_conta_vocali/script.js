7f2ed9e688cc5c8b45b6f6892db16d8f053841e2
rob587 <robertocamm02@yahoo.com>
2025-07-08 23:24:01 +0200

    quarto esercizio terminato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -6,8 +6,29 @@ const word = 'javascript';
 // Dichiara la funzione qui.
 
 
+const vocali= ['a', 'e', 'i', 'o', 'u']
+
+
+
+function vocalCount(str) {
+    let count = 0;
+    let vocaliTrovate = [];
+
+    for (let i = 0; i < str.length; i++) {
+        const lettera = str[i].toLowerCase();
+        if (vocali.includes(lettera)) {
+            count++;
+            vocaliTrovate.push(lettera);
+        } 
+        
+    }
+    console.log("Vocali trovate:", vocaliTrovate); 
+    return count; 
+}
 // Invoca la funzione qui e stampa il risultato in console
 
 
+
+console.log("Numero di vocali:", vocalCount(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
