04b29f9a7eebe314a35ee1615c3160bf48e3eb0a
rob587 <robertocamm02@yahoo.com>
2025-07-08 21:59:27 +0200

    terzo esercizio terminato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -6,11 +6,11 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 // Dichiara la funzione qui.
 
 function isInArray (names, letter){
-
+    return names.filter(name => name[0] === letter);
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(isInArray(names,'A'))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
